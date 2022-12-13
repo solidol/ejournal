f8cda0c778b7bb39e717bb354e03fabe53db8812
solidol <magnet87@gmail.com>
2022-12-13 14:47:32 +0200

    різне

--- app/Models/Absent.php
+++ app/Models/Absent.php
@@ -25,32 +25,6 @@ class Absent extends Model
         return Absent::where('kod_prepod', Auth::user()->userable_id)->where('kod_subj', $subj)->where('kod_grup', $group)->where('data_', $date)->get();
     }
 
-    public static function listByLesson($lessonId)
-    {
 
-        $lessonInfo = Lesson::find($lessonId);
-
-        return Student::where('kod_grup', $lessonInfo->kod_grupi)->orderBy('FIO_stud')->get();
-        /*
-        return DB::table('spisok_stud')->select(
-            'spisok_stud.kod_stud',
-            'spisok_stud.kod_grup',
-            'spisok_stud.FIO_stud',
-            'vidsutni.data_',
-            'vidsutni.kod_prepod',
-            'vidsutni.kod_subj',
-            'vidsutni.nom_pari',
-            'vidsutni.kod_lesson',
-            DB::raw('DATE_FORMAT(vidsutni.data_,"%d.%m.%y") as dateFormatted'),
-            DB::raw("$lessonId as lesson_id")
-        )->leftJoinSub(
-            Absent::select('kod_stud', 'kod_prepod', 'kod_subj', 'nom_pari', 'kod_lesson', 'data_')->where('kod_prepod', Auth::user()->userable_id)->where('kod_lesson', $lessonId),
-            'vidsutni',
-            function ($join) {
-                $join->on('vidsutni.kod_stud', '=', 'spisok_stud.kod_stud');
-            }
-        )->where('spisok_stud.kod_grup', $lessonInfo->kod_grupi)->orderBy('FIO_stud')->get();
-*/
-    }
 
 }
