6e889fb32a1ab1a1f52c3167f7e209175d38b710
solidol <magnet87@gmail.com>
2023-01-06 16:25:24 +0200

    логи

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -18,9 +18,6 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
-        //'Illuminate\Auth\Events\Login' => [
-        //    'App\Listeners\AuthLoginEventHandler',
-        //],
     ];
 
     /**
