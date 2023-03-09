cb11948628c8a8e6607e430075f2fe84b0eebc15
solidol <magnet87@gmail.com>
2023-03-09 15:25:16 +0200

    control

--- app/Models/Control.php
+++ app/Models/Control.php
@@ -39,11 +39,6 @@ class Control extends Model
         return $this->hasMany(Mark::class, 'control_id')->where('kod_stud', '>', 0);
     }
 
-    public function onlyMarks()
-    {
-        return $this->marks()->where('kod_stud', '>', 0)->get();
-    }
-
     public function mark($student_id)
     {
         return $this->marks->where('kod_stud', $student_id)->first() ?? ((object) array('mark_str' => '', 'ocenka' => '0'));
