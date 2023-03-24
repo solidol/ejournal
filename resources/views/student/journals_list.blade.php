80c741354e74a5dc1f55293442e3d1de60c54778
solidol <magnet87@gmail.com>
2023-03-24 17:45:41 +0200

    baloon

--- resources/views/student/journals_list.blade.php
+++ resources/views/student/journals_list.blade.php
@@ -5,16 +5,14 @@
 
 @section('sidebar')
 
-<h2 class="d-sm-none d-md-block">Оцінки з інших дисциплін</h2>
-
-
-<nav class="nav flex-column d-none d-md-block">
-    @foreach($journals as $journal)
-    <a class="nav-link" href="{{URL::route('student_get_marks',['id'=>$journal->id])}}">{{$journal->group->nomer_grup}} - {{$journal->subject->subject_name}}</a>
-    @endforeach
-</nav>
-
-
+<div class="baloon">
+    <h2>Оцінки з інших дисциплін</h2>
+    <nav class="nav flex-column">
+        @foreach($journals as $journal)
+        <a class="nav-link" href="{{URL::route('student_get_marks',['id'=>$journal->id])}}">{{$journal->group->nomer_grup}} - {{$journal->subject->subject_name}}</a>
+        @endforeach
+    </nav>
+</div>
 @stop
 
 
