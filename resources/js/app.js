f3c7ea6aab5aa3f9c3ec9adecda71c6d6a3436ce
solidol <magnet87@gmail.com>
2023-03-31 22:53:04 +0300

    preloader

--- resources/js/app.js
+++ resources/js/app.js
@@ -12,10 +12,11 @@ $(document).ready(function () {
 
 });
 
+
 $(window).on('load', function () {
     $('body').addClass('loaded_hiding');
     window.setTimeout(function () {
         $('body').addClass('loaded');
         $('body').removeClass('loaded_hiding');
-    }, 500);
+    }, 800);
 });
