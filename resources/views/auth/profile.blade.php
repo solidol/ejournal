c93b25f21f400174ba64d6b27d7c56e4ee2372b5
solidol <magnet87@gmail.com>
2023-04-23 22:31:12 +0300

    йцуй

--- resources/views/auth/profile.blade.php
+++ resources/views/auth/profile.blade.php
@@ -3,9 +3,9 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-6">
+        <div class="col-md-8">
             <div class="card">
-                <div class="card-header  text-white bg-dark">Вхід у журнал</div>
+                <div class="card-header  text-white bg-dark">Мій профіль</div>
 
                 <div class="card-body row">
                     <div class="col-4">
