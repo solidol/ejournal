d1a19c11c59d954c59de0d0b7613ff830e6767cf
solidol <magnet87@gmail.com>
2023-04-25 14:08:10 +0300

    uio

--- resources/views/curator/student_profile.blade.php
+++ resources/views/curator/student_profile.blade.php
@@ -16,48 +16,6 @@
 
 <h2>Логін {{ $student->user->name }}</h2>
 
-<h2>Дисципліни</h2>
-
-<div class="mb-3 mt-1 table-responsive">
-
-    <table class="table table-striped table-bordered">
-        <thead>
-            <tr>
-                <th>
-                    Назва
-                </th>
-                <th>
-                    Оцінки
-                </th>
-                <th>
-                    Пропуски
-                </th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($student->group->journals as $journal)
-            <tr>
-                <td>
-                    {{$journal->subject->title}}
-                </td>
-                <td>
-
-                </td>
-
-                <td>
-                    <a class="btn btn-success pt-0 pb-0" href="#">
-                        <i class="bi bi-pencil-square"></i>
-                    </a>
-
-                </td>
-            </tr>
-            @endforeach
-        </tbody>
-
-    </table>
-
-</div>
-
 <h2>Відвідував журнал</h2>
 
 <div class="mb-3 mt-1 table-responsive">
