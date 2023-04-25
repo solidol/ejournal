765b6bf46df518405d249f6617d64211f060017b
solidol <magnet87@gmail.com>
2023-04-25 14:26:52 +0300

    dddd

--- resources/views/menus/main_teacher.blade.php
+++ resources/views/menus/main_teacher.blade.php
@@ -13,7 +13,7 @@
                 <!-- Authentication Links -->
 
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ route('get_journals') }}"><i class="bi bi-book"></i> <span>Журнали</span></a>
+                    <a class="nav-link" href="{{ route('get_journals') }}"><i class="bi bi-book"></i> <span class="d-md-inline d-lg-none">Журнали</span></a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="{{ route('find_student') }}"><i class="bi bi-search"></i> <span class="d-md-inline d-lg-none">Пошук студента</span></a>
@@ -53,14 +53,14 @@
 
                 @if (Auth::user()->isCurator())
                 <li class="nav-item">
-                    <a class="nav-link" href="/curator" class="btn btn-outline-success"><i class="bi bi-pencil-square"></i> Журнали куратора</a>
+                    <a class="nav-link" href="/curator" class="btn btn-outline-success"><i class="bi bi-pencil-square"></i> <span class="d-md-inline d-lg-none">Журнали куратора</span></a>
                 </li>
                 @endif
 
 
                 @if (Auth::user()->isAdmin())
                 <li class="nav-item">
-                    <a class="nav-link" href="/admin" class="btn btn-outline-success"><i class="bi bi-gear"></i></a>
+                    <a class="nav-link" href="/admin" class="btn btn-outline-success"><i class="bi bi-gear"></i> <span class="d-md-inline d-lg-none">Адмінпанель</span></a>
                 </li>
                 @endif
             </ul>
