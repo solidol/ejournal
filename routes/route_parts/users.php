f4ef013e7c018afa5d93e07856703a448c22c9f8
solidol <magnet87@gmail.com>
2023-10-07 22:34:54 +0300

    dsfdsf

--- routes/route_parts/users.php
+++ routes/route_parts/users.php
@@ -7,9 +7,9 @@ use Illuminate\Http\Request;
 
 Route::group(['middleware' => 'admin'], function () {
 
-        Route::get('/admin/users/list/{slug?}', [UserController::class, 'index'])->name('users.index');
+        Route::get('/admin/users/{slug?}', [UserController::class, 'index'])->name('users.index');
 
-        Route::post('/admin/users/store', [UserController::class, 'WUStore'])->name('users.store');
+        Route::post('/admin/users', [UserController::class, 'WUStore'])->name('users.store');
 
         Route::get('/admin/users/loginas/{user}', [UserController::class, 'loginAs'])->name('users.loginas');
     });
