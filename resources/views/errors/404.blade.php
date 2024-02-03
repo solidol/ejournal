ec8c28b34327dd8296922e51d2ed80deadac8db1
solidol <magnet87@gmail.com>
2024-02-03 13:19:45 +0200

    timezone

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,18 +1,26 @@
 @extends('layouts.app-simple')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-6">
-            <div class="card">
-                <div class="card-header  text-white bg-dark">Щось не так</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-header  text-white bg-dark">Щось не так</div>
 
-                <div class="card-body">
-                    <h1>Помилка 404</h1>
-                    <p>Сторінку не знайдено!</p>
+                    <div class="card-body">
+                        <h1>Помилка 404</h1>
+                        <p>
+                            Сторінку не знайдено!
+                        </p>
+                        <p>
+                            {{ date('d.m.Y H:i:s') }}
+                        </p>
+                        <p>
+                            {{ date_default_timezone_get() }}
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
--- resources/views/journals/index.blade.php
+++ resources/views/journals/index.blade.php
@@ -4,97 +4,99 @@
 @section('side-title', 'Мої дисципліни')
 
 @section('sidebar')
-<div class="baloon">
-    <h2>Новини та повідомлення</h2>
-    @foreach ($messages as $mesItem)
-    <div class="mb-3 mt-3 p-2 border border-2 border-primary rounded-2">
-        <p class="text-danger fs-4">{{$mesItem->content}}</p>
+    <div class="baloon">
+        <h2>Новини та повідомлення</h2>
+        @foreach ($messages as $mesItem)
+            <div class="mb-3 mt-3 p-2 border border-2 border-primary rounded-2">
+                <p class="text-danger fs-4">{{ $mesItem->content }}</p>
+            </div>
+        @endforeach
     </div>
-    @endforeach
-</div>
 
 @stop
 
 @section('content')
-<h1>
-    Мої дисципліни
-    <button type="button" class="mb-1 mt-1 btn btn-danger" data-bs-toggle="modal" data-bs-target="#addLesson">
-        Новий журнал
-    </button>
-</h1>
+    <h1>
+        Мої дисципліни
+        <button type="button" class="mb-1 mt-1 btn btn-danger" data-bs-toggle="modal" data-bs-target="#addLesson">
+            Новий журнал
+        </button>
+    </h1>
 
-<div class="mb-3 mt-1 table-responsive">
+    <div class="mb-3 mt-1 table-responsive">
 
-    <table class="table table-striped table-bordered">
-        <thead>
-            <tr>
-                <th>
-                    Група
-                </th>
-                <th>
-                    Назва
-                </th>
-                <th>
-                    Вич.
-                </th>
-                <th>
-                    Відкрити
-                </th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($journals as $journal)
-            <tr>
-                <td class="text-center">
-                    <div class="btn text-white py-0" style="background-color: <?= $journal->color ?? '#000' ?>;">
-                        {{$journal->group?$journal->group->nomer_grup:''}}
-                    </div>
-                </td>
-                <td>
-                    {{$journal->subject->subject_name}} 
-                    @if ($journal->parent)
-                    <i class="bi bi-vr fs-5"></i>
-                    @endif
-                </td>
-                <td>
-                    {{$journal->lessons->sum('kol_chasov')}} год.
-                </td>
-                <td>
+        <table class="table table-striped table-bordered">
+            <thead>
+                <tr>
+                    <th>
+                        Група
+                    </th>
+                    <th>
+                        Назва
+                    </th>
+                    <th>
+                        Вич.
+                    </th>
+                    <th>
+                        Відкрити
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($journals as $journal)
+                    <tr>
+                        <td class="text-center">
+                            <div class="btn text-white py-0" style="background-color: <?= $journal->color ?? '#000' ?>;">
+                                {{ $journal->group ? $journal->group->nomer_grup : '' }}
+                            </div>
+                        </td>
+                        <td>
+                            {{ $journal->subject->subject_name }}
+                            @if ($journal->parent)
+                                <i class="bi bi-vr fs-5"></i>
+                            @endif
+                        </td>
+                        <td>
+                            {{ $journal->lessons->sum('kol_chasov') }} год.
+                        </td>
+                        <td>
 
-                    <a class="btn btn-success py-0" href="{{URL::route('lessons.index',['id'=>$journal->id])}}">
-                        <i class="bi bi-pencil-square"></i> Пари
-                    </a>
-                    <a class="btn btn-success py-0" href="{{URL::route('marks.index',['id'=>$journal->id])}}">
-                        <i class="bi bi-5-square"></i> Оцінки
-                    </a>
+                            <a class="btn btn-success py-0 m-1"
+                                href="{{ URL::route('lessons.index', ['id' => $journal->id]) }}">
+                                <i class="bi bi-pencil-square"></i> <span class="d-md-inline d-none">Пари</span>
+                            </a>
+                            <a class="btn btn-primary py-0 m-1" href="{{ URL::route('marks.index', ['id' => $journal->id]) }}">
+                                <i class="bi bi-5-square"></i> <span class="d-md-inline d-none">Оцінки</span>
+                            </a>
 
-                    <a class="btn btn-success py-0" href="{{URL::route('journals.show',['journal'=>$journal])}}">
-                        <i class="bi bi-book"></i> Журнал
-                    </a>
-                </td>
-            </tr>
-            @endforeach
-        </tbody>
-        <tfoot>
-            <tr>
-                <th>
+                            <a class="btn btn-danger py-0 m-1"
+                                href="{{ URL::route('journals.show', ['journal' => $journal]) }}">
+                                <i class="bi bi-book"></i> <span class="d-md-inline d-none">Журнал</span>
+                            </a>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+            <tfoot>
+                <tr>
+                    <th>
 
-                </th>
-                <th>
+                    </th>
+                    <th>
 
-                </th>
-                <th>
+                    </th>
+                    <th>
 
-                </th>
-                <th>
+                    </th>
+                    <th>
 
-                </th>
-            </tr>
-        </tfoot>
-    </table>
+                    </th>
+                </tr>
+            </tfoot>
+        </table>
 
-</div>
+    </div>
 
-@include('journals.popups.create')
+    @include('journals.popups.create')
 
 @stop
